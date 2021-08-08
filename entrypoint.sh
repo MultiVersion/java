203d2913ca74d6b1812e3ec7581145f5bc85eb26
Dviih <dviih@dviih.email>
2021-08-08 20:55:09 -0300

    Updated Smart2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ sed -i "s/${D_VARIANT,,}-$D_MINECRAFT_VERSION.jar/$SERVER_JARFILE/g" /home/conta
 if [[ ! $(md5sum -c md5sums.txt 2>/dev/null | grep OK$ | awk '{print $2}') == "OK" ]]; then
   wget -q https://multiversion.dviih.software/${D_VARIANT,,}-$D_MINECRAFT_VERSION.jar -O $SERVER_JARFILE
 fi
-if [ "$D_MINECRAFT_VERSION" \> "1.16" ]; then
+if [[ "$(echo "$D_MINECRAFT_VERSION" | cut -d "." -f 2)" -ge "$(echo "1.17" | cut -d "." -f 2)" ]]; then
   java16 -version
   java16 -Xms64M -Xmx${SERVER_MEMORY}M -jar $SERVER_JARFILE
 else
