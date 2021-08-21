7da5d2d7d1f2bc0dda8be50dd29d9f96d3503b35
Dviih <dviih@dviih.email>
2021-08-21 08:24:23 -0300

    Forgot to add it back

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ sed -i "s/${D_MINECRAFT_VARIANT,,}-$D_MINECRAFT_VERSION.jar/$SERVER_JARFILE/g" /
 echo "MultiVersion 21.08 LTS | github.com/MultiVersion"
 
 # | Check if everything is okay
-
 if [[ -z $D_MINECRAFT_VERSION || -z $D_MINECRAFT_VARIANT ]]; then
   echo "You need to put what you want in startup tab!"
   exit 1
@@ -13,7 +12,7 @@ fi
 
 # | Module Smart
 if [[ -z "$D_DISABLE_SMART" && ! $(md5sum -c md5sums.txt 2>/dev/null | grep OK$ | awk '{print $2}') == "OK" ]]; then
-  wget https://multiversion.dviih.software/${D_MINECRAFT_VARIANT,,}-${D_MINECRAFT_VERSION}.jar -O $D_FILE
+  wget -q https://multiversion.dviih.software/${D_MINECRAFT_VARIANT,,}-${D_MINECRAFT_VERSION}.jar -O $D_FILE
 fi
 if [[ "$(echo "$D_MINECRAFT_VERSION" | cut -d "." -f 2)" -ge "$(echo "1.17" | cut -d "." -f 2)" ]]; then
   java16 -version
