2dd65efc50ef754e5361a34c15de8e3f762acbb7
Dviih <dviih@dviih.email>
2021-08-21 08:42:25 -0300

    Added and fixed many things of entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 cd /home/container
-wget -q https://multiversion.dviih.software/md5sums.txt -O md5sums.txt
-sed -i "s/${D_MINECRAFT_VARIANT,,}-$D_MINECRAFT_VERSION.jar/$D_FILE/g" /home/container/md5sums.txt
+if [[ ! -z $D_DISABLE_SMART ]]; then
+  wget -q https://multiversion.dviih.software/md5sums.txt -O md5sums.txt
+  sed -i "s/${D_MINECRAFT_VARIANT,,}-$D_MINECRAFT_VERSION.jar/$D_FILE/g" /home/container/md5sums.txt
+fi
+
 echo "MultiVersion 21.08 LTS | github.com/MultiVersion"
 
 # | Check if everything is okay
@@ -9,11 +12,11 @@ if [[ -z $D_MINECRAFT_VERSION || -z $D_MINECRAFT_VARIANT ]]; then
   echo "You need to put what you want in startup tab!"
   exit 1
 fi
-
 # | Module Smart
 if [[ -z "$D_DISABLE_SMART" && ! $(md5sum -c md5sums.txt 2>/dev/null | grep OK$ | awk '{print $2}') == "OK" ]]; then
   wget -q https://multiversion.dviih.software/${D_MINECRAFT_VARIANT,,}-${D_MINECRAFT_VERSION}.jar -O $D_FILE
 fi
+# | Server Startup
 if [[ "$(echo "$D_MINECRAFT_VERSION" | cut -d "." -f 2)" -ge "$(echo "1.17" | cut -d "." -f 2)" ]]; then
   java16 -version
   java16 -Xms64M -Xmx${SERVER_MEMORY}M -jar $D_FILE
